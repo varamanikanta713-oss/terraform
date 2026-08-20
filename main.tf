dfc73d9b42bd2fdcca6f21ec5edd67b284e1193a
varamanikanta713@gmail.com <varamanikanta713@gmail.com>
2026-08-20 13:04:01 +0530

    file2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
      region = "ap-south-2"
 }
-resource "aws_instance" "name" {
+resource "aws_instance" "rama" {
      ami = "ami-0795a1d68c086ce01"
      instance_type = "t3.micro"
      key_name = "MANI"
