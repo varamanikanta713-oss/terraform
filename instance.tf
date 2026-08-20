279ad8899f8134ddc077a8f0aeba7ef5c3fe83a5
varamanikanta713@gmail.com <varamanikanta713@gmail.com>
2026-08-20 10:58:57 +0530

    tf

---
+++ instance.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+     region = "ap-south-2"
+}
+resource "aws_instance" "name" {
+     ami = "ami-0795a1d68c086ce01"
+     instance_type = "t3.micro"
+     key_name = "MANI"
+     tags = {
+     Name = "raju"
+     } 
+}
